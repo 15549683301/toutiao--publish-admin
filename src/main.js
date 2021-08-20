ffc21c164b7b52f45fab427ac29596335c58f87a
橘子 <862601220@qq.com>
2021-08-20 20:18:40 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
-
+// 创建Vue根实例
+// 把router配置到2跟实例中
+// 通过render方法把App根组件渲染到#app入口节点
 new Vue({
   router,
   render: h => h(App)
